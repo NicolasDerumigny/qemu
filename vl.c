de9c85b558c66a84c22af8e18b5d7bef9f3270be
Dr. David Alan Gilbert <dgilbert@redhat.com>
2019-02-27 16:49:00 +0000

    migration: Cleanup during exit
    Currently we cleanup the migration object as we exit main after the
    main_loop finishes; however if there's a migration running things
    get messy and we can end up with the migration thread still trying
    to access freed structures.
    
    We now take a ref to the object around the migration thread itself,
    so the act of dropping the ref during exit doesn't cause us to lose
    the state until the thread quits.
    
    Cancelling the migration during migration also tries to get the thread
    to quit.
    
    We do this a bit earlier; so hopefully migration gets out of the way
    before all the devices etc are freed.
    
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>
    Tested-by: Alex Bennée <alex.bennee@linaro.org>
    Message-Id: <20190227164900.16378-1-dgilbert@redhat.com>
    Reviewed-by: Juan Quintela <quintela@redhat.com>
    Reviewed-by: Alex Bennée <alex.bennee@linaro.org>
    Signed-off-by: Dr. David Alan Gilbert <dgilbert@redhat.com>

--- vl.c
+++ vl.c
@@ -4619,6 +4619,12 @@ int main(int argc, char **argv, char **envp)
 
     gdbserver_cleanup();
 
+    /*
+     * cleaning up the migration object cancels any existing migration
+     * try to do this early so that it also stops using devices.
+     */
+    migration_shutdown();
+
     /* No more vcpu or device emulation activity beyond this point */
     vm_shutdown();
 
@@ -4634,7 +4640,6 @@ int main(int argc, char **argv, char **envp)
     monitor_cleanup();
     qemu_chr_cleanup();
     user_creatable_cleanup();
-    migration_object_finalize();
     /* TODO: unref root container, check all devices are ok */
 
     return 0;
